42f0bcf9be2e9c31a319b14bd45f8fb9b8043d15
Vitalie Lazu <vitalie@debian.md>
2012-12-10 23:11:15 +0200

    Added dashboard controller and default page will be dashboard#index

--- config/routes.rb
+++ config/routes.rb
@@ -74,7 +74,7 @@ Crm::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'task_reports#index'
+  root :to => 'dashboard#index'
 
   # See how all your routes lay out with "rake routes"
 
