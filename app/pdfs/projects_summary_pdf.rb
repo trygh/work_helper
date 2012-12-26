9f307737ba40d2188865cbb001795c0b12df520d
alexwebs <alexwebscom@gmail.com>
2012-12-26 14:09:21 +0200

    working version

--- app/pdfs/projects_summary_pdf.rb
+++ app/pdfs/projects_summary_pdf.rb
@@ -4,6 +4,7 @@ class ProjectsSummaryPdf < Prawn::Document
     super()
     @report = report
     @view = view
+
     line_items
     # [ "Project", "Worker", "Total Hours", "Hourly Rates", "Total" ],
     # table( [ ["Project", "Worker", "Total Hours", "Hourly Rates", "Total" ] ] )
@@ -21,29 +22,35 @@ class ProjectsSummaryPdf < Prawn::Document
   def line_items
     move_down 20
     table line_item_rows do
-      row(0).font_style = :bold
-      # columns(1..5).align = :right
-      self.row_colors = ["DDDDDD", "FFFFFF"]
-      self.header = true
+        row(0).font_style = :bold
+        row(0).align = :center
+        # columns(1..5).align = :center
+        self.row_colors = ["DDDDDD", "FFFFFF"]
+        self.header = true
     end
   end
 
   def line_item_rows
-    [["Project", "Worker", "Total Hours", "Hourly Rates", "Total"]] +
+    result = [["Project", "Worker", "Total Hours", "Hourly Rates", "Total"]]
+    rows = []
+
     @report.projects_summary.map do |project_id, stats|
-      [@report.project_name(project_id),
+      result << [{content: @report.project_name(project_id), font_style: :bold, colspan: 5}]
+
       stats.map do |row|
-        [row[0], row[1], row[2], row[3] ]
+        # rows << [row[1], row[2], row[3]]
+        result << [{content: row[0], colspan: 2, align: :left, padding: [5, 5, 5, 100]}, row[1], row[2], row[3] ]
       end
-    ]
+      # if stats.size > 1
+      #   result << [{content: '', colspan: 4, content: "Subtotal:"}, {content: "#{@report.project_amount(project_id)}", align: :right, font_style: :bold}]
+      # end
+
+        result << [{content: '', colspan: 4, content: "Subtotal:"}, {content: "#{@report.project_amount(project_id)}", align: :right, font_style: :bold}]
     end
+
+    # #1455C0 blue
+    result << [{content: '', colspan: 4, content: "Total:"}, { content: "#{@report.total_amount}", align: :right, font_style: :bold} ]
+
+    result
   end
-
-  #   def line_item_rows
-  #   [["Product", "Qty", "Unit Price", "Full Price"]] +
-  #   @order.line_items.map do |item|
-  #     [item.name, item.quantity, price(item.unit_price), price(item.full_price)]
-  #   end
-  # end
-
 end
